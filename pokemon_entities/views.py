899a0959ebc0c53dac18f1fddc05cfcfe28db53c
yk <yefim_korshever@mail.ru>
2022-08-17 22:37:47 +0300

    Show only actual pokemons

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,6 +3,7 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from django.utils.timezone import localtime
 
 from pokemon_entities.models import Pokemon, PokemonEntity
 
@@ -29,8 +30,11 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-
-    pokemon_entities = PokemonEntity.objects.all()
+    now = localtime()
+    pokemon_entities = PokemonEntity.objects.filter(
+        appeared_at__lte=now,
+        disappeared_at__gte=now
+    )
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in pokemon_entities:
         add_pokemon(
